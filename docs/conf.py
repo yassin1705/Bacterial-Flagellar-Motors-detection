1a2a602b4a8018f99638c2e0980165ca5c12d852
yassin1705 <112560465+yassin1705@users.noreply.github.com>
2025-05-21 17:43:22 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+project = 'Tornado Trajectory Project'
+copyright = '2025'
+author = 'yassine'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    # 'myst_parser',  # Uncomment if you're using Markdown files
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
